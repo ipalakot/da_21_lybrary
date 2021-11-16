e4ead74a2f584b7ba99eedcfcbb21fed3bc9711d
ipalakot <ipalakot@gmail.com>
2021-11-16 13:14:26 +0100

    unicity

--- src/Entity/Utilisateurs.php
+++ src/Entity/Utilisateurs.php
@@ -5,9 +5,11 @@ namespace App\Entity;
 use App\Repository\UtilisateursRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
  * @ORM\Entity(repositoryClass=UtilisateursRepository::class)
+ * @UniqueEntity("email")
  */
 class Utilisateurs
 {
