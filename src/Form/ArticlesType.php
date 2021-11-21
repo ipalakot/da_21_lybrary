5dafd684646f8ba48fa9d6bbac97defc4da77472
ipalakot <ipalakot@gmail.com>
2021-11-21 19:12:46 +0100

    Form Correction

--- src/Form/ArticlesType.php
+++ src/Form/ArticlesType.php
@@ -3,26 +3,45 @@
 namespace App\Form;
 
 use App\Entity\Articles;
-//use App\Entity\Categories;
-
+use App\Entity\Categories;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-//use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\DateType;
 
 class ArticlesType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
+            ->add('title', 
+                TextType::class, [
+
+                    'label'=> 'Titre',
+                ])
 
             ->add('contenu')
-            ->add('category')
-            ->add('date')
+            ->add('category', EntityType::class, [
+                // Label du champ    
+                'label'  => 'Categorie',
+        
+                // looks for choices from this entity
+                'class' => Categories::class,
+            
+                // Sur quelle propriete je fais le choix
+                'choice_label' => 'titre',
+            
+                // used to render a select box, check boxes or radios
+                // 'multiple' => true,
+                //'expanded' => true,
+            ])
+
+            ->add('date',            
+                DateType::class, 
+                ['label'=> 'Date'])
         ;
     }
 
@@ -32,4 +51,5 @@ class ArticlesType extends AbstractType
             'data_class' => Articles::class,
         ]);
     }
+    
 }
