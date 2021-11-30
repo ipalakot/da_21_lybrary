d6258f86dc2d422bf6c780baea4d6204ada757ce
ipalakot <ipalakot@gmail.com>
2021-11-30 10:45:49 +0100

    final Autenticator

--- src/Controller/AdminUserController.php
+++ src/Controller/AdminUserController.php
@@ -3,7 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\User;
-use App\Form\User1Type;
+use App\Form\UserType;
 use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -32,7 +32,7 @@ class AdminUserController extends AbstractController
     public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
         $user = new User();
-        $form = $this->createForm(User1Type::class, $user);
+        $form = $this->createForm(UserType::class, $user);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
@@ -63,7 +63,7 @@ class AdminUserController extends AbstractController
      */
     public function edit(Request $request, User $user, EntityManagerInterface $entityManager): Response
     {
-        $form = $this->createForm(User1Type::class, $user);
+        $form = $this->createForm(UserType::class, $user);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -6,17 +6,28 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 
 class UserType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('roles')
-            ->add('password')
             ->add('username')
-        ;
+            ->add('email')
+            ->add('password', PasswordType::class)
+            ->add('roles', ChoiceType::class, array(
+                'choices' => array(
+                    'Utilisateur'=> 'ROLE_USER',
+                    'Editeur'=> 'ROLE_EDITOR',
+                    'Moderateur'=> 'ROLE_MODERA',
+                    'Administrateur'=> 'ROLE_ADMIN',
+                ),
+                'expanded'=>true,
+                'multiple'=>true,
+            ))
+            ;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
