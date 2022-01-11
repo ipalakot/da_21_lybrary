bd0b1b41c37baa1c98d02773e36d40bfc7510f73
ipalakot <ipalakot@gmail.com>
2022-01-11 09:45:39 +0100

    End

--- config/bundles.php
+++ config/bundles.php
@@ -18,4 +18,5 @@ return [
     Liip\ImagineBundle\LiipImagineBundle::class => ['all' => true],
     FOS\CKEditorBundle\FOSCKEditorBundle::class => ['all' => true],
     FM\ElfinderBundle\FMElfinderBundle::class => ['all' => true],
+    Liip\TestFixturesBundle\LiipTestFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
--- tests/Repository/UserRepositoryTest.php
+++ tests/Repository/UserRepositoryTest.php
@@ -2,18 +2,22 @@
 
 namespace App\Tests\Repository;
 
+use Liip\TestFixturesBundle\Test\FixturesTrait;
+use App\DataFixtures\UserFixtures;
 use App\Repository\UserRepository;
-use AW\HmacBundle\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
 class UserRepositoryTest extends KernelTestCase
 {
+    use FixturesTrait;
+    
     public function testCalcul(): void
     {
         # Le besoin ici est de de recuperer mon Repo 
         # Pour cela, je je Lance le kermel afin d'avoir le noyau
         // $kernel = self::bootKernel();
             self::bootKernel();
+            $this->loadFixtures([UserFixtures::class]);
 
         # Maintenant que j'ai le noyau je peux acceder au Container
          // $kermel->getContainer();
